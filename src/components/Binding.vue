96d2918899a2077e8ea49a9e01f9d6dc757e3c66
Andre Santiago <andresantosas2558487@gmail.com>
2021-09-28 12:54:36 -0300

    estudo de diretiva v-bind

--- src/components/Binding.vue
+++ src/components/Binding.vue
@@ -4,8 +4,12 @@
     <h1>Binding method</h1>
    {{ menssagem }}
    <hr>
+   <h3>Usando v-bind</h3>
    <input v-bind:value="menssagem" type="text" @keyup="alterarMenssagem">
-   <input :value="menssagem" type="text" @keyup.enter="alterarMenssagem">
+   <input :value="menssagem" type="text" @keyup.enter="alterarMenssagem"> <br><br>
+   <hr>
+   <h3>Usando v-model</h3>
+   <input v-model="menssagem" type="text">
    <hr>
    <a target="blank" :href="url">Link</a> para o site do vue
    <hr>
